76ce4d4d30c6961326a8aa9d815afeee1dd61d54
Andrew Quartey <yestrup@gmail.com>
2019-06-24 21:15:28 +0000

    code refactor

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Component } from 'react';
 import LinkedList from './datastructures/LinkedList';
 import './App.css';
 import BinarySeachTree from './datastructures/BinarySearchTree';
-import HT from './HashTable';
+import HT from './datastructures/HashTable';
 
 class App extends Component {
   render() {
